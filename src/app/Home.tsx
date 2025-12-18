b8f6c0e053e47f4db9b158491f9d2776324d976c
018medina <018.medina@gmail.com>
2025-12-18 15:29:50 -0300

    melhorias

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,15 +1,23 @@
 import { View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+import { StackRoutesList } from "@/routes/StackRoutes";
 
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { ButtonIcon } from "@/components/ButtonIcon";
 
-export function Home() {
+type Props = NativeStackScreenProps<StackRoutesList, "home">;
+
+export function Home({ navigation }: Props) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <Title>Home</Title>
-        <ButtonIcon name="add-circle" />
+        <ButtonIcon
+          name="add-circle"
+          onPress={() => navigation.navigate("product")}
+        />
       </Header>
     </View>
   );
